1b73a34351496fbccd78256847f876d66d7c8044
valeriadmitrievna1506 <valeriadmitrievna1506@gmail.com>
2020-09-16 12:26:42 +0300

    header and first

---
+++ js/script.js
@@ -0,0 +1,26 @@
+function pageSwitcher(el) {
+  page = el.id;
+
+  switch (page) {
+    case "EvaMat":
+      //Загрузить определенную страницу
+      alert(page);
+      break;
+    case "3Dmat":
+      //Загрузить определенную страницу
+      alert(page);
+      break;
+    case "Alcantar":
+      //Загрузить определенную страницу
+      alert(page);
+      break;
+    case "Fur":
+      //Загрузить определенную страницу
+      alert(page);
+      break;
+    case "Trunk":
+      //Загрузить определенную страницу
+      alert(page);
+      break;
+  }
+}
